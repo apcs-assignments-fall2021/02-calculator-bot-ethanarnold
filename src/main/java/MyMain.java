7965c2b15fe884705760aedcf020014691baee84
ethanarnold <emailtoethan@gmail.com>
2021-09-08 21:35:20 -0700

    calc bot

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,19 +4,24 @@ public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        if(b>a) {
+            if(b<c) return b;
+            return a;
+        }
+    return c;
     }
 
-    // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if(Math.abs(a) > Math.abs(b)) return a;
+        return b;
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        return Math.sqrt(a*a+b*b);
     }
+
 
     public static void main(String[] args) {
         // You may want to keep these lines of code to test that your methods work
@@ -25,6 +30,29 @@ public class MyMain {
         System.out.println(pythagoras(3, 4)); // should be 5.0
 
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("median, magnitude, or pythagoras?");
+        if(scan.next().equalsIgnoreCase("median")) {
+            System.out.println("give first number");
+            int a = scan.nextInt();
+            System.out.println("give second number");
+            int b = scan.nextInt();
+            System.out.println("give third number");
+            int c = scan.nextInt();
+            System.out.println(median(a, b, c));
+        }
+        if(scan.next().equalsIgnoreCase("magnitude")) {
+            System.out.println("give first number");
+            int a = scan.nextInt();
+            System.out.println("give second number");
+            int b = scan.nextInt();
+            System.out.println(magnitude(a, b));
+        }
+        if(scan.next().equalsIgnoreCase("pythagoras")) {
+            System.out.println("give first number");
+            int a = scan.nextInt();
+            System.out.println("give second number");
+            int b = scan.nextInt();
+            System.out.println(pythagoras(a, b));
+        }
     }
 }
